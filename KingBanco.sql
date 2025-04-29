2c3fc218d00f2e9cb01c4fd9cf6aaaf6a9c02818
Erik <139016787+ErikCarvalho1@users.noreply.github.com>
2025-04-29 08:09:27 -0300

    Update KingBanco.sql

--- KingBanco.sql
+++ KingBanco.sql
@@ -9,7 +9,8 @@ CREATE TABLE Usuarios (
     Ativo BIT DEFAULT 1,
     criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
+INSERT INTO Usuarios (Id, Nome, Cpf_cnpj, Email, Tipo_usuario, Senha, Ativo)
+VALUES (1, 'Erik', '123.456.789-00', 'erik@erik', 'Administrador', md5('123'), 1);
 -- create table Cadastro(
 -- Nome VARCHAR(100) NOT NULL,
 -- Cpf_cnpj VARCHAR(20), 
@@ -20,8 +21,19 @@ CREATE TABLE Usuarios (
 create table Login (
 Id INT primary key,
 Usuario_id int,
-Email VARCHAR(150) NOT NULL UNIQUE,
-Senha Varchar (30),
+
 FOREIGN KEY (Usuario_Id) REFERENCES Usuarios(Id),
 Logado_em DATETIME DEFAULT CURRENT_TIMESTAMP   
 );
+create cliente (
+id
+eee
+FOREIGN KEY (Usuario_Id) REFERENCES Usuarios(Id),
+)
+
+
+select * from usuarios; 
+SELECT * FROM usuarios WHERE email = 'erik@erik' AND senha = md5('123');
+
+update usuarios set senha = '123' where id = 1;
+update usuarios set senha = '123' where id = 1;
