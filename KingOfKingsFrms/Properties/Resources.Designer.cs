b5dd16715191cb6d73c6b375d87fa5cf0e914dec
ERIK CARVALHO DA SILVA <erik.csilva7@senacsp.edu.br>
2025-05-07 11:40:00 -0300

    Adiciona formulários e recursos para o projeto
    Implementadas classes e métodos para `FormProdutos` e `FormVenda`, incluindo a configuração de propriedades essenciais. Criados os arquivos de recursos `FormProdutos.resx` e `FormVenda.resx` com a estrutura XML necessária. Adicionado o recurso `leaoFundo` em `Resources.Designer.cs` e atualizado `Resources.resx` com novos recursos, como `dog-removebg-preview` e `linha em pe dorada`. Alterações na configuração do `DataGridView` em `FormUsuarios.Designer.cs` e atualização correspondente em `FormUsuarios.resx`. Novo recurso binário `leaoFundo.png` adicionado.

--- KingOfKingsFrms/Properties/Resources.Designer.cs
+++ KingOfKingsFrms/Properties/Resources.Designer.cs
@@ -203,6 +203,16 @@ namespace KingOfKingsFrms.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap leaoFundo {
+            get {
+                object obj = ResourceManager.GetObject("leaoFundo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap LeaoLogin {
             get {
                 object obj = ResourceManager.GetObject("LeaoLogin", resourceCulture);
